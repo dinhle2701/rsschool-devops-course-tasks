5c55d5d4cd19c25e42d37954ff80069562dd0654
dinh-letat <dinhprococoa@gmail.com>
2025-03-24 15:08:49 +0700

    fix: update iam

--- iam.tf
+++ iam.tf
@@ -1,38 +1,60 @@
-data "aws_iam_policy_document" "github_oidc_trust" {
-  statement {
-    effect  = "Allow"
-    actions = ["sts:AssumeRoleWithWebIdentity"]
-    principals {
-      type        = "Federated"
-      identifiers = ["arn:aws:iam::851725458360:oidc-provider/token.actions.githubusercontent.com"]
-    }
-    condition {
-      test     = "StringLike"
-      variable = "token.actions.githubusercontent.com:sub"
-      values   = ["repo:${var.github_repo}:ref:refs/heads/main"]
-    }
-  }
-}
-
 resource "aws_iam_role" "github_actions_role" {
-  name               = "GitHubActionsRole"
-  assume_role_policy = data.aws_iam_policy_document.github_oidc_trust.json
-}
+  name = "GithubActionsRole"
 
-resource "aws_iam_policy" "github_actions_policy" {
-  name        = "GitHubActionsPolicy"
-  description = "Policy for GitHub Actions to access AWS resources"
-  policy      = jsonencode({
+  assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Action   = "s3:*"
-      Effect   = "Allow"
-      Resource = "${aws_s3_bucket.terraform_state.arn}/*"
-    }]
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = "arn:aws:iam::851725458360:oidc-provider/token.actions.githubusercontent.com"
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+          "ForAnyValue:StringLike" = {
+            "token.actions.githubusercontent.com:sub" = "repo:dinhle2701/rsschool-devops-course-tasks:*"
+          }
+        }
+      }
+    ]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "github_actions_attachment" {
+# Attach existing AWS managed policies
+resource "aws_iam_role_policy_attachment" "ec2_full_access" {
   role       = aws_iam_role.github_actions_role.name
-  policy_arn = aws_iam_policy.github_actions_policy.arn
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "route53_full_access" {
+  role       = aws_iam_role.github_actions_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonRoute53FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "s3_full_access" {
+  role       = aws_iam_role.github_actions_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "iam_full_access" {
+  role       = aws_iam_role.github_actions_role.name
+  policy_arn = "arn:aws:iam::aws:policy/IAMFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "vpc_full_access" {
+  role       = aws_iam_role.github_actions_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonVPCFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "sqs_full_access" {
+  role       = aws_iam_role.github_actions_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSQSFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "eventbridge_full_access" {
+  role       = aws_iam_role.github_actions_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess"
 }
